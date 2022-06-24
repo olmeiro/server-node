66e3980461b318b578d01d5bba44c4da5d214589
Olmeiro orozco <olme.orozco@gmail.com>
2022-06-24 10:49:59 -0500

    set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+        "jest": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "no-console": "warn"
+    }
+}
